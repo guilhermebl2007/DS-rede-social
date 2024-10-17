33f748153779961b5703080bb8bac121e5f784a7
guilhermebl2007 <101270753+guilhermebl2007@users.noreply.github.com>
2024-10-17 11:52:05 -0300

    fix2.0 Aula 10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -29,14 +29,3 @@ async function visualizarInformacoesGlobais() {
 visualizarInformacoesGlobais()
 
 /*Aula 04 */
-const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
-  }
-  
-  const tickConfig = {
-    color: getCSS('--primary-color'),
-    size: 16,
-    family: getCSS('--font')
-  }
-  
-  export { getCSS, tickConfig }
